fed4ad45691307deb527b95f0a2e15c775e92eea
AmineHLub <Amino.bhl@gmail.com>
2021-12-23 14:56:44 +0100

    Add tests for : book,person,student

---
+++ student.test.rb
@@ -0,0 +1,24 @@
+require_relative './student'
+require_relative './person'
+
+describe Person do
+  before :each do
+    @student = Student.new('class A', 18, 'Max', true)
+  end
+
+  it 'shows instance of student class' do
+    expect(@student).to be_instance_of Student
+  end
+
+  it 'matches student name' do
+    expect(@student.name).to match 'Max'
+  end
+
+  it 'matches the age' do
+    expect(@student.age).to match 18
+  end
+
+  it 'matches the class' do
+    expect(@student.classroom).to match 'class A'
+  end
+end
